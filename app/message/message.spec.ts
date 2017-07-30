a733f81593dd6a965e6681767e11b3411c076f85
iamthes <s91630277@gmail.com>
2017-07-30 05:38:40 +0400

    smoke save

--- app/message/message.spec.ts
+++ app/message/message.spec.ts
@@ -1,6 +1,8 @@
 import { MessageModel } from './message.model';
 import * as assert from 'assert';
 import { Message } from './message';
+import { openConnection, closeConnection } from '../connection';
+import { getEntityManager, getRepository } from 'typeorm';
 
 describe('message:', () => {
 
@@ -26,4 +28,26 @@ describe('message:', () => {
 
     });
 
+    describe('smoke save', () => {
+
+        before(async () => {
+            await openConnection();
+        });
+
+        after(async () => {
+            await closeConnection();
+        });
+
+        it('smoke save', async () => {
+            let entity: Message = {
+                author: 'Launa',
+                body: 'Foo',
+            };
+            let model = new MessageModel(entity);
+            let m = Object.assign(new Message(), model, {dateInserted: '2015-02-01'});
+            const repository = getRepository(Message);
+            await repository.save(m);
+        });
+
+    });
 });
